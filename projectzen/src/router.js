a808150d73f1bfcdf1545f798aba64c3cdf775ca
Rabbit591 <dees-85@yandex.ru>
2024-12-19 20:25:29 +0300

    profilePage

--- projectzen/src/router.js
+++ projectzen/src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/homeView.vue';
 import Login from './components/logForm.vue';
 import Register from './components/regForm.vue';
-import Profile from './views/regView.vue';
+import Profile from './views/profile.vue';
 import Project_page from './views/project_page.vue';
 
 const routes = [
