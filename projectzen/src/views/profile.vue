52efbfaf696994d70e4b712ed549bc968bbfdc5e
Rabbit591 <dees-85@yandex.ru>
2024-12-19 22:35:54 +0300

    working avatar

--- projectzen/src/views/profile.vue
+++ projectzen/src/views/profile.vue
@@ -34,6 +34,7 @@
       console.log(selectedFile);
     }
       const uploadPI = async () => {
+        console.log(selectedFile, user.value.id,user.value.type)
         if (!selectedFile) {
         alert('Сначала выберите изображение');
         return;
@@ -54,6 +55,7 @@
         selectedFile,
         uploadPI,
         handleFileChange,
+        profileIconUrl
       };
     }
   }
